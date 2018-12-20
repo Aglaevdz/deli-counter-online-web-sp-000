0cae0efa7fc8667b848cbba0b28c7af28507a530
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-20 00:28:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  line_spot = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    line_spot.each_with_index(0) do |name, index|
+      line_spot.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_spot.join(" ")}"
+    end
+end
