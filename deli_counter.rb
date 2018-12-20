80b3074c4d32d7c490e50be77e1835b465df01fd
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-20 00:50:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.each_index {|name, index|}
+    spot.each_index {|name, index|}
       spot.push("#{index}. #{name}")
     end
     puts "The line is currently: #{spot.join(" ")}"
